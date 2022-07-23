10768e2be072bb273449a29f23a9084e770fc85f
Brendan Tang <b@brendantang.net>
2022-07-23 04:23:20 -0400

    try catch in postgres call

--- backend/articles/get.ts
+++ backend/articles/get.ts
@@ -4,20 +4,20 @@ import notFound from "../notFound.ts";
 
 export default function getArticle(pool: postgres.Pool): RouteHandler {
   return async function (_req, _connInfo, params): Promise<Response> {
-    //try {
-    const db = await pool.connect();
-    const id = params["id"];
-    const result = await db.queryObject<Article>(
-      "SELECT id, slug, title, body, created_at, updated_at FROM articles WHERE id = $ID",
-      { id: id },
-    );
+    try {
+      const db = await pool.connect();
+      const id = params["id"];
+      const result = await db.queryObject<Article>(
+        "SELECT id, slug, title, body, created_at, updated_at FROM articles WHERE id = $ID",
+        { id: id },
+      );
 
-    const article = result.rows[0];
+      const article = result.rows[0];
 
-    return json({ article: article });
-    //} catch (e) {
-    // console.error(e);
-    //return notFound();
-    // }
+      return json({ article: article });
+    } catch (e) {
+      console.error(e);
+      return notFound();
+    }
   };
 }
