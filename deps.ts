70c8f5c7c61741681c94e72ec250ab599a387b64
Brendan Tang <b@brendantang.net>
2022-07-23 21:08:12 -0400

    Fix deps.ts

--- deps.ts
+++ deps.ts
@@ -10,7 +10,7 @@ export {
   type Routes,
   serve,
   timeoutAfter,
-} from "../deno_framework/mod.ts";
-//} from "https://raw.githubusercontent.com/brendantang/routing-framework/main/mod.ts";
+  //} from "../deno_framework/mod.ts";
+} from "https://raw.githubusercontent.com/brendantang/routing-framework/main/mod.ts";
 
 export * as postgres from "https://deno.land/x/postgres@v0.16.1/mod.ts";
